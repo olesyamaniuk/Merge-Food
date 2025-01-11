411691ca91f71f20cc382b206263c36fe4d84da5
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-11 15:56:01 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -1,19 +1,4 @@
-$(function () {
-  $('.how-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.how-list-left',
-    nextArrow: '.how-list-right',
-  });
-});
+
 
 $(function () {
   $('.gallery__list').slick({
