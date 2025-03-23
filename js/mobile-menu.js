3937ca653a7af0db123f1d195a42b7f00fadeec4
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-03-23 20:26:55 +0200

    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -34,27 +34,6 @@ function toggleAnswer(element) {
   }
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const popup = document.getElementById("cookie-popup");
-//   const acceptButton = document.getElementById("cookies");
-//   const declineButton = document.getElementById("decline-cookies");
-//   const closeButton = document.getElementById("close-popup");
-
-//   if (!localStorage.getItem("cookiesAccepted")) {
-//     popup.style.display = "block";
-//   }
-//   acceptButton.onclick = function () {
-//     localStorage.setItem("cookiesAccepted", "true");
-//     popup.style.display = "none";
-//   };
-//   declineButton.onclick = function () {
-//     localStorage.setItem("cookiesAccepted", "false");
-//     popup.style.display = "none";
-//   };
-//   closeButton.onclick = function () {
-//     popup.style.display = "none";
-//   };
-// });
 document.addEventListener("DOMContentLoaded", function () {
   const popup = document.getElementById("cookie-popup");
   const acceptButton = document.getElementById("accept-cookies");
